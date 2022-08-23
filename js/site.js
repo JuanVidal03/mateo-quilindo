cfbaa7024784ecf6ca9766b7f8fd2fd9db8a08db
Juan Vidal <juanmvg2003@gmail.com>
2022-08-23 16:29:53 -0500

    fueron añadidas librerias vegas y jquery, se empezó a desarrollar la sección de inicio

---
+++ js/site.js
@@ -0,0 +1,12 @@
+//slider principal
+$("#contenedor-vegas").vegas({
+    delay: 5000,
+    animation: "random",
+    cover: true,
+    overlay: '/vendor/vegas/overlays/02.png',
+    slides: [
+        { src: "../img/mateo-1.webp" },
+        { src: "../img/mateo-8.webp" },
+        { src: "../img/mateo-9.webp" }
+    ]
+});

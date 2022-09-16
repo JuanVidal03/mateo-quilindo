e3dff45c76f4da995c2816c7acaa7ff496a34c13
Juan Vidal <juanmvg2003@gmail.com>
2022-09-16 18:39:03 -0500

    consumiendo API poemas y diseñando layout para la sección 'poemas'

--- js/site.js
+++ js/site.js
@@ -1,7 +1,10 @@
 //Here are the API with some poems
 const poemasUrl = '../poemas.json';
 
-//getting DOM
+
+//getting data from DOM
+const poemasContainer = document.getElementById('poemas__container');
+
 
 //function gets data
 const poemas = async() =>{
@@ -9,6 +12,22 @@ const poemas = async() =>{
     const resp = await fetch(poemasUrl);
     const data = await resp.json();
 
+    data.forEach(poema => {
+        
+        const div = document.createElement('div');
+        div.classList.add('card__poemas');
+        console.log(div);
+    
+        let title = document.createElement('h5')
+        title.innerHTML = ` Hola ${poema.poema}`;
+        console.log(title);
+        poemasContainer.appendChild(div);
+        div.appendChild(title);
+    });
+
+    /*
+    
+    */
 
 
 }
